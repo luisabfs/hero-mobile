46317d62548e03ee116c62ad4c9b9933b4d39255
Luisa Barros <luisa.barros.dev@gmail.com>
2023-04-27 21:23:07 -0300

    refactor: truncate hero name if too long

--- src/components/HeroItem/index.tsx
+++ src/components/HeroItem/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { HeroData } from '../../hooks/useFetchHeroes';
-import { treatThumbnailUri } from '../../utils';
+import { treatThumbnailUri, truncateString } from '../../utils';
 import { Font } from '../../styles';
 import { Container, Thumbnail } from './styles';
 
@@ -8,14 +8,14 @@ interface Props {
     item: HeroData;
 }
 
-const CharacterItem: React.FC<Props> = ({ item }) => {
+const HeroItem: React.FC<Props> = ({ item }) => {
   const { path, extension } = item.thumbnail;
   return (
     <Container>
         <Thumbnail source={{uri: treatThumbnailUri({ path, extension })}}  />
-        <Font size={21} color="#4e4e4e">{item.name}</Font>
+        <Font size={21} color="#4e4e4e">{truncateString(item.name, 20)}</Font>
     </Container>
     );
 }
 
-export default CharacterItem;
+export default HeroItem;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,6 @@
-export const treatThumbnailUri = ({ path = '', extension = ''}): string => {
+export const treatThumbnailUri = ({ path = '', extension = ''}): string | undefined => {
+  if(!path || !extension) return;
+
   const formattedPath = path.replace("http", "https");
   return `${formattedPath}.${extension}`;
 }
@@ -6,4 +8,9 @@ export const treatThumbnailUri = ({ path = '', extension = ''}): string => {
 export function groupArrayByThree([a,b,c,...rest]: number[]): Array<number[]>{
   if (rest.length === 0) return [[a,b,c].filter(x => x!==undefined)]
   return [[a,b,c]].concat(groupArrayByThree(rest))
+}
+
+export const truncateString = (string: string, limit = 100): string | undefined => {
+  if(!string) return;
+  return `${string.slice(0, limit)}${string.length > limit ? '...' : ''}`;
 }
